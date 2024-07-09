394155a29fb08e893cd9ca3be7dafac46522b4fe
Tejas Lagwankar <tejlagwankar1999@gmail.com>
2024-07-09 15:18:19 +0530

    explore menu list

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Explore a varied menu showcasing a delightful selection of dishes made with premium ingredients and culinary mastery. Our goal is to fulfill your appetite and enhance your dining experience with each meal. </p>
+            <button>View Menu</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
